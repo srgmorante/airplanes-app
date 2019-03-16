8ee022d6e3be2e690cc9c1a98a0e17eba5f37849
Sergio Morante <sergio.morante@soprasteria.com>
2019-03-16 14:02:02 +0100

    feature: adds the header and the navigation hablity, creates the main containers to navigate to them

---
+++ src/01-containers/airports/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const airportsContainer = () => (
+  <React.Fragment>
+    <h1>This is the airports container</h1>
+  </React.Fragment>
+);
+
+export default airportsContainer;
---
+++ src/01-containers/home/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const homeContainer = () => (
+  <span>This is the home</span>
+)
+
+export default homeContainer;
