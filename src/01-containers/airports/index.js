7e2e1356650dc687a4bad6412a61bdb08b708986
Sergio Morante <sergio.morante@soprasteria.com>
2019-03-16 18:37:29 +0100

    feature: implements the table component on airports container and connects it with redux

--- src/01-containers/airports/index.js
+++ src/01-containers/airports/index.js
@@ -1,9 +1,34 @@
-import React from 'react';
+// Core
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+// Components
+import Table from '../../00-components/table-component';
+// Styles
+import './index.css'
+// Actions
+import { loadAirports } from '../../02-actions/airports';
 
-const airportsContainer = () => (
-  <React.Fragment>
-    <h1>This is the airports container</h1>
-  </React.Fragment>
-);
+class AirportsContainer extends Component {
+  componentDidMount() {
+    this.props.loadAirports();
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Spain Airports</h1>
+        <Table
+          className="airports-table"
+          dataColumns={['name', 'country', 'city', 'geolocation']}
+          dataRows={this.props.airportList}
+        />
+      </React.Fragment>
+    )
 
-export default airportsContainer;
+  }
+};
+
+export default connect((state) => {
+  return {
+    airportList: state.airport.airportList
+  }
+}, {loadAirports})(AirportsContainer);
