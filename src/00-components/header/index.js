86c5c817edc555ef3511375b30ed48eb82a34454
Sergio Morante <sergio.morante@soprasteria.com>
2019-03-17 20:12:12 +0100

    feat: changes to NavLink to style the menu items when they are actives

--- src/00-components/header/index.js
+++ src/00-components/header/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import  { Link } from 'react-router-dom';
+import  { NavLink } from 'react-router-dom';
 
 import './index.css';
 
 const headerComponent = () => (
-  <header className="App-header">
-    <Link to="/"><span role="img" aria-label="plane">✈️</span></Link>
+  <header>
+      <NavLink to="/"><span className="logo" role="img" aria-label="plane">✈️</span></NavLink>
       <nav className="menu">
         <ul>
-          <li><Link to="/airports">Airports</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
+          <li><NavLink to="/airports">Airports</NavLink></li>
+          <li><NavLink to="/contact">Contact</NavLink></li>
         </ul>
       </nav>
   </header>
